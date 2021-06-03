3d8289c1434f18c7f2182737d60e0a063c6d8331
Robert <42147723+RobertKrolikowski@users.noreply.github.com>
2021-06-03 23:06:58 +0200

    Dodaj pliki projektów.

---
+++ HackerRankProblemSolving/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HackerRankProblemSolving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
