8073efa5a3264dce07ef632a9cb3e0bfc1be7693
Robert <42147723+RobertKrolikowski@users.noreply.github.com>
2021-06-06 23:50:32 +0200

    ExtraLongFactorials

---
+++ ExtraLongFactorials/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Numerics;
+
+namespace ExtraLongFactorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExtraLongFactorials(25);
+        }
+        public static void ExtraLongFactorials(int n)
+        {
+            BigInteger result = new BigInteger(1);
+            for (int i = 1; i <= n; i++)
+                result *= i;
+            Console.Write(result);
+        }
+    }
+}
