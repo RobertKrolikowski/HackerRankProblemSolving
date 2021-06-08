d8f647ccaeffe872417c9ceb18d7494404c69c87
Robert <42147723+RobertKrolikowski@users.noreply.github.com>
2021-06-08 23:20:24 +0200

    https://www.hackerrank.com/challenges/angry-professor/problem

---
+++ AngryProfessor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AngryProfessor
+{
+    class Program
+    {
+        //https://www.hackerrank.com/challenges/angry-professor/problem
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AngryProfessor(4, new List<int>() { 0, -1, 2, 1 }));
+        }
+
+        public static string AngryProfessor(int k, List<int> a)
+        {
+            return a.Where(x => x <= 0).Count() < k ? "YES" : "NO";
+        }
+    }
+}
