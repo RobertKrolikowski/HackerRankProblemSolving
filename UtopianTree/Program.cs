5905adc0e3accf55c39ad200ac635c90bd787a3f
Robert <42147723+RobertKrolikowski@users.noreply.github.com>
2021-06-08 23:04:25 +0200

    https://www.hackerrank.com/challenges/utopian-tree/problem

---
+++ UtopianTree/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UtopianTree
+{
+    class Program
+    {
+        //https://www.hackerrank.com/challenges/utopian-tree/problem
+        static void Main(string[] args)
+        {
+            Console.WriteLine(UtopianTree(5));
+        }
+
+        public static int UtopianTree(int n)
+        {
+            int result = 1;
+            for (int i = 0; i < n; i++)
+            {
+                if (i % 2 == 0)
+                    result = result * 2;
+                else
+                    result++;
+            }
+            return result;
+        }
+    }
+}
