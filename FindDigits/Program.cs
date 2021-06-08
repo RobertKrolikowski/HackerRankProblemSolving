eb96c1d36f15e4334a862b855880b0b929f7c982
Robert <42147723+RobertKrolikowski@users.noreply.github.com>
2021-06-08 23:31:51 +0200

    https://www.hackerrank.com/challenges/find-digits/problem

---
+++ FindDigits/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace FindDigits
+{
+    class Program
+    {
+        //https://www.hackerrank.com/challenges/find-digits/problem
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindDigits(124));
+        }
+
+        public static int FindDigits(int n)
+        {
+            return n.ToString().Where(x => x != '0' && n % (x - '0') == 0).Count();
+        }
+    }
+}
