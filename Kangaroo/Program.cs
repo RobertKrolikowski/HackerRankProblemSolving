c7691a288b6c50dbebf8933916096ec68a3a42ce
Robert <42147723+RobertKrolikowski@users.noreply.github.com>
2021-06-09 23:42:17 +0200

    https://www.hackerrank.com/challenges/kangaroo/problem

---
+++ Kangaroo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Kangaroo
+{
+    class Program
+    {
+        //https://www.hackerrank.com/challenges/kangaroo/problem
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kangaroo(0, 2, 5, 3));
+        }
+        public static string Kangaroo(int x1, int v1, int x2, int v2)
+        {
+            do
+            {
+                x1 += v1;
+                x2 += v2;
+                if (x1 == x2)
+                    return "YES";
+
+            } while ((x1 < x2 && v1 > v2)||(x1 > x2 && v1 < v2));
+            return "NO";
+        }
+    }
+}
