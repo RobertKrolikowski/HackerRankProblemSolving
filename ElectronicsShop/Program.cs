554604ec44d13e851ae44d073eb17b9089330085
Robert <42147723+RobertKrolikowski@users.noreply.github.com>
2021-06-10 00:04:06 +0200

    https://www.hackerrank.com/challenges/electronics-shop/problem

---
+++ ElectronicsShop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ElectronicsShop
+{
+    class Program
+    {
+        //https://www.hackerrank.com/challenges/electronics-shop/problem
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMoneySpent(new[] { 92,8 },new[]{ 7, 2, 8 },10));
+        }
+
+        static int GetMoneySpent(int[] keyboards, int[] drives, int b)
+        {
+            int max = -1;
+            for (int i = 0; i < keyboards.Length; i++)
+            {
+                for (int j = 0; j < drives.Length; j++)
+                {
+                    int sum = keyboards[i] + drives[j];
+                    if (max < sum && sum <= b)
+                        max = sum;
+
+                }
+            }
+
+            return max;
+        }
+    }
+}
