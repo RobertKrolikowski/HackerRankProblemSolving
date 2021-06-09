456b2aa6e64f5692a55fafa852b02a2d09807703
Robert <42147723+RobertKrolikowski@users.noreply.github.com>
2021-06-10 00:26:04 +0200

    https://www.hackerrank.com/challenges/strange-advertising/problem

---
+++ ViralAdvertising/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace ViralAdvertising
+{
+    class Program
+    {
+        //https://www.hackerrank.com/challenges/strange-advertising/problem
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ViralAdvertising(5));
+        }
+        public static int ViralAdvertising(int n)
+        {
+            int result = 2;
+            int shared = 5;
+            for (int i = 1; i < n; i++)
+            {
+                shared = shared / 2 * 3;
+                result += shared /2;
+            }
+            return result;
+        }
+    }
+}
