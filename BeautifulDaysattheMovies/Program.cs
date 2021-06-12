c5959178604fdcf291e454fd9be94332b29f0bbc
Robert <42147723+RobertKrolikowski@users.noreply.github.com>
2021-06-12 12:46:19 +0200

    https://www.hackerrank.com/challenges/beautiful-days-at-the-movies/problem

---
+++ BeautifulDaysattheMovies/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BeautifulDaysattheMovies
+{
+    class Program
+    {//https://www.hackerrank.com/challenges/beautiful-days-at-the-movies/problem
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BeautifulDays(1, 2000000, 23047885));
+        }
+
+        public static int BeautifulDays(int i, int j, int k)
+        {
+            int result = 0; 
+            float reverse = 0;
+            for (int a = i; a <= j; a++)
+            {
+                //char[] charArray = a.ToString().ToCharArray();
+                //Array.Reverse(charArray);
+                //reverse = float.Parse(charArray);
+                int buff = a;
+                string buff1 = "";
+                do
+                {
+                    buff1 += (buff % 10).ToString();
+                    buff = buff / 10;
+                } while (buff > 0);
+                reverse =float.Parse( buff1);
+                if (Math.Abs(reverse - a) / k % 1 == 0)
+                    result++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ FormingAMagicSquare/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FormingAMagicSquare
+{
+    class Program
+    {
+        //https://www.hackerrank.com/challenges/magic-square-forming/problem
+        static void Main(string[] args)
+        {
+            List<List<int>> arr = new List<List<int>>();
+            arr.Add(new List<int> { 5, 3, 4 });
+            arr.Add(new List<int> { 1, 5, 8 });
+            arr.Add(new List<int> { 6, 4, 2 });
+            FormingMagicSquare(arr);
+        }
+        public static int FormingMagicSquare(List<List<int>> s)
+        {
+            int result = 0;
+            for (int i = 0; i < s.Count; i++)
+            {
+                for (int j = 0; j < s.Count; j++)
+                {
+                    if (s[i][0] + s[i][1] + s[i][2] != 15)
+                    {
+                        result += Math.Abs( 15 - (s[i][0] + s[i][1] + s[i][2]));
+                        s[i][j] += 15 - (s[i][0] + s[i][1] + s[i][2]);
+                    }
+                    else if (s[0][j] + s[1][j] + s[2][j] != 15)
+                    {
+                        result += Math.Abs(15 - (s[0][j] + s[1][j] + s[2][j]));
+                        s[i][j] += 15 - (s[0][j] + s[1][j] + s[2][j]);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
